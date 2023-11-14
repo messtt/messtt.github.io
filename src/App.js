78e9247cef0cb653d48863fa605261ebfd7cded1
messtt <113893214+messtt@users.noreply.github.com>
2023-11-14 16:47:10 +0100

    [FIX] -> cursor

--- src/App.js
+++ src/App.js
@@ -15,27 +15,14 @@ import './styles/app.css';
 
 function App() {
     useEffect(() => {
-        const follower = document.querySelector('.mouse_following');
+        const cursor = document.querySelector('.mouse_following');
         const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
         if (!isMobile) {
             function updateFollowerPosition(e) {
-                gsap.to(follower, {
-                    x: e.clientX - follower.offsetWidth / 2 + window.scrollX,
-                    y: e.clientY - follower.offsetHeight / 2 + window.scrollY,
-                    filter: 'blur(10px)',
-                    display: 'block',
-                    backgroundColor: '#ffffff',
-                    duration: 0.1,
-                });
+                cursor.style.left = e.clientX + 'px';
+                cursor.style.top = e.clientY + 'px';
             }
-
-            document.addEventListener('mousemove', (e) => {
-                updateFollowerPosition(e);
-            });
-
-            document.addEventListener('scroll', (e) => {
-                updateFollowerPosition(e);
-            });
+            window.addEventListener('mousemove', updateFollowerPosition);
         }
     }, []);
 
--- src/componants/presentation.js
+++ src/componants/presentation.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './../styles/presentation.css';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import pp from './../assets/Photo_profil_Corentin_Albertus.PNG';
 
 gsap.registerPlugin(ScrollTrigger);
 function Presentation() {
@@ -12,20 +13,7 @@ function Presentation() {
         const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
         if (!isMobile) {
-            gsap.to(presentation_content, {
-                scrollTrigger: {
-                    trigger: presentation_content,
-                    start: 'top 80%',
-                    end: 'top 40%',
-                    toggleActions: 'play reverse reverse reverse',
-                    scrub: 0,
-                },
-                scale: 0.7,
-                boxShadow: '0px 0px 50px 4px #4446c1',
-                borderRadius: '30px',
 
-                duration: 1,
-            });
         }
     }, []);
 
@@ -33,17 +21,22 @@ function Presentation() {
         <div className="presentation" id="presentation">
             <div className="presentation_container">
                 <div className="presentation_title">
-                    <h1>Présentation</h1>
+                    <h1>Corentin Albertus</h1>
                 </div>
                 <div className="presentation_content_image">
-                    <img className="presentation_image" src="https://picsum.photos/200/300" alt="" />
+                    <img className="presentation_image" src={pp} alt="" />
                 </div>
                 <div className="presentation_content">
                     <div className="presentation_content_text">
                         <p className="presentation_text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Nesciunt, quibusdam
-                            quas. Quos, voluptates. Quia, quibusdam. Quisquam, quod. Nesciunt, quibusdam quas. Quos,
-                            voluptates. Quia, quibusdam.
+                            Étudiant en première année à Epitech
+                            Montpellier, je travaille principalement
+                            en C.
+                            Curieux et motivé, j'aime acquérir de
+                            nouvelles compétences au travers de
+                            projets et j'aimerais découvrir le milieu
+                            professionnel afin d'étendre mes
+                            connaissances.
                         </p>
                     </div>
                 </div>
